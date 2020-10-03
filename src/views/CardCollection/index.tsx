d4da0c2959e293b8d5d88f28a053927777bf6b93
Victor Caceres <caceres.vc95@gmail.com>
2020-10-03 17:58:44 -0400

    create toast service

--- src/views/CardCollection/index.tsx
+++ src/views/CardCollection/index.tsx
@@ -1,30 +1,7 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import Toast from 'react-native-root-toast';
+import { showToast } from '../../utils';
 import styles from './styles';
-
-const showToast = () => {
-  Toast.show('This is a message', {
-    duration: Toast.durations.LONG,
-    position: Toast.positions.BOTTOM,
-    shadow: true,
-    animation: true,
-    hideOnPress: true,
-    delay: 0,
-    onShow: () => {
-        // calls on toast\`s appear animation start
-    },
-    onShown: () => {
-        // calls on toast\`s appear animation end.
-    },
-    onHide: () => {
-        // calls on toast\`s hide animation start.
-    },
-    onHidden: () => {
-        // calls on toast\`s hide animation end.
-    }
-  });
-}
 
 export default () => {
   return (

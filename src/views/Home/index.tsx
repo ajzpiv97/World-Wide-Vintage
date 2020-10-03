ffffee38200f118d2bd76b17b080ed2d25b79496
Armando Zubillaga <ajzpiv97@gmail.com>
2020-10-03 02:15:17 -0400

    Added title and type list

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect }from 'react';
+import { Text, SafeAreaView, ScrollView, View, StatusBar, Image, FlatList} from 'react-native';
+import { human } from 'react-native-typography'
 import styles from './styles';
 
-export default () => {
-  return (
-    <View style={styles.container}>
-      <Text>Home!</Text>
-    </View>
+
+// export default () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+const Home = () => {
+
+    const [vintage, setVintage] = useState([
+      { type: 'MLB', key: '1'},
+      { type: 'NBA', key: '2'},
+      { type: 'NFL', key: '3'},
+      { type: 'Pokemon', key: '4'},
+    ])
+
+
+    return (
+    <SafeAreaView style={styles.container}>
+      <View><StatusBar barStyle={'dark-content'}/></View>
+      <View style={styles.homeImage}>
+        <Text style={human.largeTitle}>World Wide Vintage</Text>
+      </View>
+      <ScrollView horizontal={true}>
+        {
+          vintage.map((item) => {
+            return (
+              <View key={item.key}>
+                <Text style={styles.types}>{item.type}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+export default Home;
--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -2,9 +2,32 @@ const styles: any = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'stretch',
+  },
+
+  homeImage: {
+    height: 100,
+    backgroundColor: '#a37eba',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+
+  homeTitle: {
+    fontSize: 48
+
+  },
+
+  statusBar: {
+    backgroundColor: 'black'
+  },
+
+  types: {
+    padding: 25,
+    fontSize: 20
+  },
+
 }
 
 export default styles;
+

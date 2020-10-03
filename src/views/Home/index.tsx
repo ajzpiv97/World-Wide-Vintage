b9e14ed97ad7b9b28635479c68c47c384a1760c6
Armando Zubillaga <ajzpiv97@gmail.com>
2020-10-03 15:44:18 -0400

    Created layout

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,42 +1,88 @@
-import React, { useState, useEffect }from 'react';
-import { Text, SafeAreaView, ScrollView, View, StatusBar, Image, FlatList, TouchableHighlight} from 'react-native';
-import { human } from 'react-native-typography'
-import * as Font from 'expo-font';
-import styles from './styles';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  SafeAreaView,
+  ScrollView,
+  View,
+  StatusBar,
+  Image,
+  FlatList,
+  TouchableHighlight,
+} from "react-native";
+import { human } from "react-native-typography";
+import * as Font from "expo-font";
+import styles from "./styles";
 
 const Home = () => {
+  const [vintage, setVintage] = useState([
+    { type: "MLB", key: "1" },
+    { type: "NBA", key: "2" },
+    { type: "NFL", key: "3" },
+    { type: "Pokemon", key: "4" },
+  ]);
 
-    const [vintage, setVintage] = useState([
-      { type: 'MLB', key: '1'},
-      { type: 'NBA', key: '2'},
-      { type: 'NFL', key: '3'},
-      { type: 'Pokemon', key: '4'},
-    ]);
+  const [selected, setSelected] = useState("1");
 
-    const [selected, setSelected] = useState('1');
-
-
-    return (
+  return (
     <SafeAreaView style={styles.container}>
-      <View><StatusBar barStyle={'dark-content'}/></View>
+      <View>
+        <StatusBar barStyle={"dark-content"} />
+      </View>
       <View style={styles.homeImage}>
         <Text style={human.largeTitle}>World Wide Vintage</Text>
       </View>
-      <ScrollView>
+      <ScrollView style={{ paddingLeft: 15 }}>
         <View style={styles.typesSpace}>
-        {
-          vintage.map(({ key, type }) => {
+          {vintage.map(({ key, type }) => {
             return (
               <View key={key}>
-                <Text onPress={() => setSelected(key)} style={[styles.typesText, { color: selected == key ? 'black' : 'grey' }]}>{type}</Text>
+                <Text
+                  onPress={() => setSelected(key)}
+                  style={[
+                    styles.typesText,
+                    { color: selected == key ? "black" : "grey" },
+                  ]}
+                >
+                  {type}
+                </Text>
               </View>
-            )
-          })
-        }
+            );
+          })}
+        </View>
+        <View>
+          <VintageOptions type={"Popular Vintage"} />
+          <VintageOptions type={"New Vintage"} />
+          <VintageOptions type={"Highest Bid"} />
+          <VintageOptions type={"Lowest Ask"} sty/>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 export default Home;
+
+const VintageOptions = (props: { type: React.ReactNode }) => {
+  return (
+    <View style={{paddingBottom: 15}}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+          }}
+        >
+          {props.type}
+        </Text>
+        <Text style={styles.seeAll}>See All {">"}</Text>
+      </View>
+      <ScrollView style={styles.options} horizontal={true}>
+        <Text style={styles.text}></Text>
+      </ScrollView>
+    </View>
+  );
+};
--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -29,9 +29,25 @@ const styles: any = {
   },
 
   typesText: {
-    padding: 25,
+    paddingRight: 15,
+    paddingTop: 15,
+    paddingBottom: 40,
     fontSize: 20
   },
+
+  options: {
+    backgroundColor: 'red',
+    height: 150
+  },
+
+  seeAll: {
+    fontSize: 15,
+    color: 'grey',
+    position: 'absolute',
+    right: 0,
+    top: 6,
+    paddingRight: 15
+  }
 
 
 }

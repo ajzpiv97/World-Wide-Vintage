5ebca12f17d632b1f98ad1b25defc9706a15c538
Victor Caceres <caceres.vc95@gmail.com>
2020-10-03 17:04:08 -0400

    add toast

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { RootSiblingParent } from 'react-native-root-siblings';
 import Main from './src/views/Main';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <Main/>
-    </NavigationContainer>
+    <RootSiblingParent>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Main/>
+      </NavigationContainer>
+    </RootSiblingParent>
   );
 }
--- src/views/CardCollection/index.tsx
+++ src/views/CardCollection/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import Toast from 'react-native-root-toast';
 import styles from './styles';
+
+const showToast = () => {
+  Toast.show('This is a message', {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.BOTTOM,
+    shadow: true,
+    animation: true,
+    hideOnPress: true,
+    delay: 0,
+    onShow: () => {
+        // calls on toast\`s appear animation start
+    },
+    onShown: () => {
+        // calls on toast\`s appear animation end.
+    },
+    onHide: () => {
+        // calls on toast\`s hide animation start.
+    },
+    onHidden: () => {
+        // calls on toast\`s hide animation end.
+    }
+  });
+}
 
 export default () => {
   return (
     <View style={styles.container}>
       <Text>Card Collection!</Text>
+      <TouchableOpacity activeOpacity={0.5} style={{ backgroundColor: 'red', marginTop: 2 }} onPress={() => showToast()}>
+        <Text>Press me</Text>
+      </TouchableOpacity>
     </View>
   );
 }

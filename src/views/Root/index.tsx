95095db14e321cac352a516b639c5e85fefae035
Victor Caceres <caceres.vc95@gmail.com>
2020-10-03 20:41:02 -0400

    fade out animation

--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import Main from '../Main';
 import Rocket from '../Rocket';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+const config: any = {
+  animation: 'timing',
+  config: {
+    duration: 2000
+  },
+};
+
+const forFade = ({ current }: any) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
+
 export default () => {
   return (
-    <Stack.Navigator headerMode={'none'} initialRouteName="rocket">
-      <Stack.Screen name="rocket" component={Rocket} />
-      <Stack.Screen name="main" component={Main} />
+    <Stack.Navigator headerMode="none" initialRouteName="rocket">
+      <Stack.Screen
+        name="rocket"
+        component={Rocket}
+      />
+      <Stack.Screen
+        name="main"
+        component={Main}
+        options={{
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+          cardStyleInterpolator: forFade
+        }}
+      />
     </Stack.Navigator>
   );
 }
